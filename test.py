3e75494a593177c0f8ded220e70d868f2654320f
juliaeis <n3WOGGm>
2017-02-21 12:43:35 +0100

    oggm dems have resolution of 40 m

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 from oggm import workflow,cfg
 import salem
+
 if __name__ == '__main__':
     cfg.initialize()
     base_dir = 'C:\\Users\\Julia\\OGGM_wd\\CentralEurope\\1000-2000'
-    #base_dir='/home/juliaeis/Dokumente/OGGM/work_dir/Alaska/land-terminating'
+    # base_dir = '/home/juliaeis/Dokumente/OGGM/work_dir/Alaska/land-terminating'
     cfg.PATHS['working_dir'] = base_dir
-    #cfg.PATHS['dem_file']='/home/juliaeis/Dokumente/OGGM/work_dir/Alaska/dem.tif'
-    RGI_FILE=base_dir+'/outlines.shp'
-    RUN_DIVIDES=False
+    # cfg.PATHS['dem_file'] = '/home/juliaeis/Dokumente/OGGM/work_dir/Alaska/dem.tif'
+    RGI_FILE = base_dir+'/outlines.shp'
+    RUN_DIVIDES = False
+
+    # set dem to 40 meters
+    cfg.PARAMS['d1'] = 40
+    cfg.PARAMS['dmax'] = 40
 
     rgidf = salem.read_shapefile(RGI_FILE, cached=True)
-    gdirs = workflow.init_glacier_regions(rgidf)
+    gdirs = workflow.init_glacier_regions(rgidf, reset=True)
     print (len(gdirs))
