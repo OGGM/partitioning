2b96d8205f7acd6a0fc83c8208d3f5ee729a8834
juliaeis <jeis@uni-bremen.de>
2017-03-21 15:00:42 +0100

    plot all divided glaciers!
    use multiprocessing is important for glacier_mask

--- scripts_for_linux/plot_figures.py
+++ scripts_for_linux/plot_figures.py
@@ -7,16 +7,16 @@ import pickle
 import os
 import shutil
 from partitioning import dividing_glaciers
+from oggm.workflow import execute_entity_task
 
 if __name__ == '__main__':
     cfg.initialize()
     base_dir = '/home/juliaeis/Dokumente/OGGM/work_dir/Central_Europe_all'
-    cfg.PATHS['working_dir'] = base_dir
+    cfg.PATHS['working_dir'] = os.path.join(base_dir, 'no_partitioning')
     cfg.PATHS['topo_dir'] = '/home/juliaeis/Dokumente/OGGM/input_data/topo'
     cfg.PARAMS['divides_gdf'] = gpd.GeoDataFrame()
     cfg.PARAMS['use_multiprocessing'] = False
     RGI_FILE = os.path.join(base_dir, '11_rgi50_CentralEurope.shp')
-    ALL_DIVIDES = os.path.join(base_dir, 'CentralEurope_all_divides.shp')
 
     # set dem to 40 meters
     cfg.PARAMS['d1'] = 40
@@ -32,25 +32,29 @@ if __name__ == '__main__':
                'RGI50-11.03828', 'RGI50-11.03829', 'RGI50-11.03830',
                'RGI50-11.03831', 'RGI50-11.03832', 'RGI50-11.03833',
                'RGI50-11.03834', 'RGI50-11.03835', 'RGI50-11.03836']
-
+    failed_topo = ['RGI50-11.00548', 'RGI50-11.03396']
     rgidf = salem.read_shapefile(RGI_FILE, cached=True)
     ID_s = pickle.load(open(os.path.join(base_dir, 'divided.pkl')))
-    print len(ID_s)
-    indices = [(i in ID_s) for i in rgidf.RGIId]
+    #ID_s = ['RGI50-11.03957']
+
+    indices = [((i in ID_s) and (i not in failed_topo)) for i in rgidf.RGIId]
     gdirs_orig = workflow.init_glacier_regions(rgidf[indices], reset=False)
 
-    for gdir_orig in gdirs_orig:
+    cfg.PATHS['working_dir'] = base_dir
+    gdirs = workflow.init_glacier_regions(rgidf[indices], reset=False)
 
-        # plot figure before partitioning
+    task_list = [
+        tasks.glacier_masks,
+        tasks.compute_centerlines]
+    for task in task_list:
+        execute_entity_task(task, gdirs_orig)
+        execute_entity_task(task, gdirs)
 
-        tasks.glacier_masks(gdir_orig)
-        tasks.compute_centerlines(gdir_orig)
-        graphics.plot_centerlines(gdir_orig)
-
-        cfg.PATHS['working_dir'] = base_dir
-        index = [i in [gdir_orig.rgi_id] for i in rgidf.RGIId]
-        gdirs = workflow.init_glacier_regions(rgidf.iloc[index])
-        plt.show()
-
-
-
+    for i, gdir_orig in enumerate(gdirs_orig):
+        fig = plt.figure(figsize=(20, 10))
+        ax0 = fig.add_subplot(1, 2, 2)
+        ax1 = fig.add_subplot(1, 2, 1)
+        graphics.plot_centerlines(gdir_orig, ax=ax1)
+        graphics.plot_centerlines(gdirs[i], ax=ax0)
+        plt.savefig(os.path.join(base_dir, 'plots', str(gdir_orig.rgi_id) + '.png'))
+        plt.close()
