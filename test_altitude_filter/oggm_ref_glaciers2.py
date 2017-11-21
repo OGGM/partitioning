0212adc32fa7b88e596fb9676d36553be6f0f3f8
juliaeis <jeis@uni-bremen.de>
2017-11-21 10:12:29 +0100

    changes

--- test_altitude_filter/oggm_ref_glaciers2.py
+++ test_altitude_filter/oggm_ref_glaciers2.py
@@ -1,5 +1,5 @@
 from oggm import workflow, cfg, tasks, graphics
-from oggm.workflow import execute_entity_task
+from oggm.core.gis import _check_geometry
 import salem
 import geopandas as gpd
 import pickle
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import subprocess
 from shapely.geometry import Point
 import os
+import numpy as np
+import copy
 
 
 if __name__ == '__main__':
@@ -20,7 +22,7 @@ if __name__ == '__main__':
 
     # check the paths
     base_dir = '/home/juliaeis/Dokumente/OGGM/work_dir/partitioning_v2'
-    rgi_file = '/home/juliaeis/Dokumente/rgi50/11_rgi50_CentralEurope/11_rgi50_CentralEurope.shp'
+    rgi_file = '/home/juliaeis/Dokumente/rgi60/11_rgi60_CentralEurope/11_rgi60_CentralEurope.shp'
     cfg.PATHS['topo_dir'] = '/home/juliaeis/Dokumente/OGGM/input_data/topo'
     cfg.PATHS['working_dir'] = base_dir
     #
@@ -30,19 +32,20 @@ if __name__ == '__main__':
     cfg.PARAMS['fixed_dx'] = 40
     cfg.PARAMS['border'] = 10
 
-    rgi = ['RGI50-11.00897']
+    rgi = ['RGI60-11.00897']
     rgidf = salem.read_shapefile(rgi_file, cached=True)
-    indices = [(i in rgi) for i in rgidf.RGIId]
+    print(list(rgidf.columns.values))
+    #indices = [(i in rgi) for i in rgidf.RGIId]
 
     #gdirs = workflow.init_glacier_regions(rgidf, reset=False)
-    gdirs = workflow.init_glacier_regions(rgidf[indices], reset=False)
+    gdirs = workflow.init_glacier_regions(rgidf, reset=False)
 
     all_divides = rgidf
 
     for gdir in gdirs:
         if gdir.rgi_region in ['11']:
-            input_shp = gdir.get_filepath('outlines', div_id=0)
-            input_dem = gdir.get_filepath('dem', div_id=0)
+            input_shp = gdir.get_filepath('outlines')
+            input_dem = gdir.get_filepath('dem')
 
             print(gdir.rgi_id)
 
@@ -50,20 +53,88 @@ if __name__ == '__main__':
             script = '/home/juliaeis/Documents/LiClipseWorkspace/partitioning-fork/test_altitude_filter/run_divides.py'
             #print(python+' ' + script + ' ' + input_shp + ' ' + input_dem + ' ' + filter_area + ' ' +filter_alt_range + ' ' + filter_perc_alt_range)
 
-            n = subprocess.call(python+' ' + script + ' ' + input_shp + ' ' + input_dem + ' ' +
-                                        str(filter_area) + ' ' + str(filter_alt_range) + ' ' + str(filter_perc_alt_range), shell=True) #+ ' > /dev/null')
+            #n = subprocess.call(python+' ' + script + ' ' + input_shp + ' ' + input_dem + ' ' +
+             #                           str(filter_area) + ' ' + str(filter_alt_range) + ' ' + str(filter_perc_alt_range), shell=True) #+ ' > /dev/null')
+            os.system(python + ' ' + script + ' ' + input_shp + ' ' + input_dem + ' ' +  str(filter_area) + ' ' + str(filter_alt_range) + ' ' + str( filter_perc_alt_range))  # + ' > /dev/null')
             #print(gdir.rgi_id+' is divided into '+str(int(n))+' parts')
             #except:
             #    print(gdir.rgi_id,'failed')
-            divides = salem.read_shapefile(os.path.join(gdir.dir, gdir.rgi_id+'_d.shp'))
-            #divides = divides.to_crs(rgidf.crs)
-            print(divides.crs)
-            #points = gpd.GeoDataFrame(geometry=[Point(divides.loc[i, 'CenLat'], divides.loc[i, 'CenLon']) for i in divides.index])
-            points.crs = divides.crs
-            points = points.to_crs(rgidf)
-            print(points)
-            rgidf = rgidf.append(divides, ignore_index=True)
-            print(rgidf.crs)
-            sorted_rgi = rgidf.sort_values('RGIId')
-   # print(rgidf['RGIId'])
-    rgidf.to_file(os.path.join(base_dir, 'DividedGlacierInventory.shp'))
+            outline = gpd.read_file(input_shp)
+            index = rgidf[rgidf['RGIId'] == gdir.rgi_id].index
+            rgidf.loc[index, 'OGGM_Area'] = [outline.area / 10 ** 6]
+
+            if os.path.exists(os.path.join(os.path.dirname(input_shp), 'divides.shp')):
+                glaciers = gpd.read_file(os.path.join(os.path.dirname(input_shp), 'divides.shp'))
+            else:
+                continue
+            glaciers = glaciers.to_crs(rgidf.crs)
+
+            divide = copy.deepcopy(outline)
+
+            for i in range(len(glaciers) - 1):
+                divide = divide.append(outline, ignore_index=True)
+            divide['Area'] = ""
+            divide['remarks'] = " "
+
+            for i in range(len(glaciers)):
+                divide.loc[i]['geometry'] = glaciers.loc[i]['geometry']
+
+            # change RGIId
+            new_id = [divide.loc[i, 'RGIId'] + '_d' + str(i + 1).zfill(2) for i
+                      in range(len(glaciers))]
+            divide.loc[divide.index, 'RGIId'] = new_id
+            geo_is_ok = []
+            new_geo = []
+
+
+            # check geometry
+            for g, a in zip(divide.geometry, glaciers.Area):
+
+                if a < 0.01:
+                    geo_is_ok.append(False)
+                    continue
+
+                try:
+                    new_geo.append(_check_geometry(g))
+                    geo_is_ok.append(True)
+                except:
+                    geo_is_ok.append(False)
+                    rgidf.loc[index, 'remarks'] = ['geometry check failed']
+
+            divide = divide.loc[geo_is_ok]
+
+            # check area
+            divide['Area'] = ""
+
+            cor_factor = float(outline.Area) / glaciers.Area.sum()
+
+            if len(divide) < 2:
+                rgidf.loc[index, 'remarks'] = [gdir.rgi_id + ' is too small or has no valid divide...']
+
+            elif cor_factor > 1.2 or cor_factor < 0.8:
+
+                rgidf.loc[index, 'remarks'] = ['sum of areas did not correlate with RGI_area']
+
+            else:
+                area_km = cor_factor * glaciers.Area
+
+                # change cenlon,cenlat
+                cenlon = [g.centroid.xy[0][0] for g in divide.geometry]
+                cenlat = [g.centroid.xy[1][0] for g in divide.geometry]
+
+                # write data
+                divide.loc[divide.index, 'CenLon'] = cenlon
+                divide.loc[divide.index, 'CenLat'] = cenlat
+                divide.loc[divide.index, 'geometry'] = new_geo
+                divide.loc[divide.index, 'OGGM_Area'] = area_km
+                rgidf = rgidf.append(divide, ignore_index=True)
+
+    sorted_rgi = rgidf.sort_values('RGIId')
+    sorted_rgi = sorted_rgi[['Area', 'OGGM_Area', 'Aspect', 'BgnDate',
+                             'CenLat', 'CenLon', 'Connect', 'EndDate', 'Form',
+                             'GLIMSId', 'Linkages', 'Lmax', 'Name', 'O1Region',
+                             'O2Region', 'RGIId', 'Slope', 'Status', 'Surging',
+                             'TermType', 'Zmax', 'Zmed', 'Zmin', 'geometry',
+                             'min_x', 'max_x', 'min_y', 'max_y', 'remarks']]
+
+    sorted_rgi.to_file(os.path.join(base_dir, str(gdir.rgi_region)+'_DividedGlacierInventory.shp'))
