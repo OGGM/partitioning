d901b2038a81d70fe3790f37fb205e0cb888f79b
juliaeis <jeis@uni-bremen.de>
2018-10-24 14:48:50 +0200

    new HEF example

--- examples/py3_HEF.py
+++ examples/py3_HEF.py
@@ -1,6 +1,6 @@
 import os
 
-from oggm import cfg, tasks, graphics, workflow
+from oggm import cfg,  workflow
 from oggm.utils import get_demo_file
 import matplotlib.pyplot as plt
 import geopandas as gpd
@@ -8,38 +8,38 @@ import geopandas as gpd
 if __name__ == '__main__':
 
     cfg.initialize()
-    cfg.set_divides_db()
-    cfg.PARAMS['use_multiprocessing'] = False
+
+    # Set Paths for OGGM
+    cfg.PATHS['working_dir'] = 'path to working directory'
+
+
     # set dem resolution to 40 meters
     cfg.PARAMS['grid_dx_method'] = 'fixed'
     cfg.PARAMS['fixed_dx'] = 40
     cfg.PARAMS['border'] = 10
+    cfg.PARAMS['use_intersects'] = False
+    cfg.PARAMS['use_multiprocessing'] = False
 
-    entity = gpd.read_file(get_demo_file('Hintereisferner.shp'))
+    # get example shapefile initialize the model
+    entity = gpd.read_file(get_demo_file('Hintereisferner_RGI6.shp'))
     hef = workflow.init_glacier_regions(entity, reset=False)[0]
 
-    input_shp = hef.get_filepath('outlines', div_id=0)
-    input_dem = hef.get_filepath('dem', div_id=0)
-
-    # filter options
-    f_area = False
-    f_alt_range = True
-    f_perc_alt_range = True
+    input_shp = hef.get_filepath('outlines')
+    input_dem = hef.get_filepath('dem')
 
     # set paths to python 2.7 and to the partitioning package
-
     python = 'path to python 2.7'
     project = 'path to the partitioning package'
-    
-    script = os.path.join(project, 'partitioning/examples/run_divides.py')
-    os.system(python + ' ' + script + ' ' + input_shp + ' ' + input_dem + ' ' +
-              str(f_area) + ' ' + str(f_alt_range) + ' ' + str(
-              f_perc_alt_range))
 
-    print('Hintereisferner is divided into', hef.n_divides, 'parts.')
+    # run_divides with python2.7
+    script = os.path.join(project, 'examples/run_divides.py')
+    os.system(python + ' ' + script + ' ' + input_shp + ' ' + input_dem)
 
-    tasks.glacier_masks(hef)
-    tasks.compute_centerlines(hef)
-    graphics.plot_centerlines(hef)
+    divides = gpd.read_file(os.path.join(hef.dir,'divides.shp'))
+
+    print('Hintereisferner was divided in '+ str(len(divides)) + ' parts')
+
+    #plot outlines of the divides
+    divides.plot()
     plt.show()
 
